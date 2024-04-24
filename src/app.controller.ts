6cadb0279c339c91d9b2aea21620df4a7af83a41
italomoraess <italomoraes.sth@gmail.com>
2024-04-24 10:31:55 -0300

    fix: controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly) {}
+  constructor() {}
 
   @Get()
   getHello(): string {
